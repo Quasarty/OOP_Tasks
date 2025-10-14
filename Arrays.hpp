25ad1d87e17563c728fea6103730458c701eef7b
Quasar <andrey.kozhevnikov.240506@gmail.com>
2025-10-14 13:51:41 +0900

    add: Arrays.hpp

---
+++ Arrays.hpp
@@ -0,0 +1,67 @@
+//Автор: Кожевников Андрей
+//Модуль для работы с динамическими массивами
+#pragma once
+
+#include <iostream>
+#include <format>
+#include <random>
+#include <fstream>
+                                      
+using namespace std;
+
+namespace arr{
+
+/**
+ * @brief Функция выводит массив
+ * 
+ * @param array массив
+ * @param size размер массива
+ */
+void print_array(const float* array, size_t size);
+
+/**
+ * @brief Функция выводит массив, выводя максимум 10 элементов в строке
+ * 
+ * @param array массив
+ * @param size размер массива
+ */
+void print_array_by_10(const float* array, size_t size);
+
+/**
+ * @brief Заполняет массив случайными числами
+ * 
+ * @param size размер массива
+ * @param min минимальный диапозон случайных чисел
+ * @param max максимальный диапозон случайных чисел
+ * @return float* указатель на первый элемент массива
+ */
+float* fill_rand(size_t size, float min = -10.0, float max = 10.0 );
+
+/**
+ * @brief Записывает массив в текстовый файл
+ * 
+ * @param array массив
+ * @param size размер массива
+ * @param file_name название файла
+ */
+void print_to_file(const float* array, size_t size, string file_name);
+
+/**
+ * @brief Заполняет массив числами из файла, возвращает размер массива
+ * 
+ * @param array массив
+ * @param file_name название файла
+ * @return size_t размер массива
+ */
+size_t fill_from_file(float* array, string file_name);
+
+/**
+ * @brief Возвращает число, считаемое из массива по некой формуле  
+ * 
+ * @param array массив
+ * @param size размер массива
+ * @return float посчитанное по формуле число
+ */
+float get_num(float *array, size_t size);
+
+}
