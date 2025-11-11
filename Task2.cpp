19ca6daf58600176c6594d3986936436aaf86a77
Quasar <andrey.kozhevnikov.240506@gmail.com>
2025-11-11 12:09:48 +0900

    CLI

--- Task2.cpp
+++ Task2.cpp
@@ -7,10 +7,13 @@
 #include <time.h>
 #include "Arrays.hpp"
 #include <assert.h>
+#include <string>
 
 using namespace std;
 
 float const EPS = 1E-6;
+string const BRIEF = "Программа считает корень квадратный модуля произведения введеных чисел";
+
 //Функция для тестов
 void test_get_num(){
     float* test_arr1 = new float[3];
@@ -30,122 +33,41 @@ void test_get_num(){
     assert(abs( arr::get_num(test_arr3, 3) - sqrt(0.75) ) < EPS );
 }
 
-int main(){
+int main(int argc, char const *argv[]){
     //Тесты
     test_get_num();
 
-    int choice = 0;
-    cout << "Выберите способ заполнения массива:\n1. Случайными числами\n2. Ввод массива из файла\n3. Ручной ввод\n";
-    cin >> choice;
+    //Проверка аргументов 
+    if (argc == 1){
+        cout << BRIEF;
+        return 0;
+    }
+    else{
+        if (argc > 1)
+            if ( (argv[1] == string("help") ) or (argv[1] == string("--help") ) or (argv[1] == string("-h")) ){
+                    cout << BRIEF;        
+                    return 0;    
+            }
+    }
     
-    string file_name;
+
+
     float* array; 
     size_t size = 0;
-
-    switch (choice){
-    //Случайные числа
-    case 1:{
-        cout << "Введите размер массива: ";
-        cin >> size;
-        array = arr::fill_rand(size);
-        break;
-    }
-    //Ввод из файла
-    case 2:{
-        int fchoice = 0;
-        cout << "1. Ввод из текстового файла\n2. Ввод из бинарного файла\n";
-        cin >> fchoice; 
-        switch (fchoice){
-        case 1:
-            cout << "Введите название файла: ";
-            cin >> file_name; 
-            try{
-                size = arr::fill_from_file(array, file_name);
-            }
-            catch(const runtime_error error){
-                cout << error.what() << '\n';
-                return 1;
-            }
-            break;
-        case 2:
-            cout << "Введите название файла: ";
-            cin >> file_name; 
-            try{
-                size = arr::fill_from_bin_file(array, file_name);
-            }
-            catch(const runtime_error error){
-                cout << error.what() << '\n';
-                return 1;
-            }
-            break;
-        default:
-            cout << "Некорректный ввод";
+    size = argc-1;
+    array = new float[size];
+    //Заполнение массива
+    for (size_t i = 1; i < argc; i++ ){
+        try{
+            array[i-1] = stof(string(argv[i]));
+        }
+        catch(const logic_error error){
+            cout << "Error: "<<error.what() << "\n";
             return 1;
         }
-        break;
     }
-    //Ручной ввод
-    case 3:{
-        cout << "Введите размер массива: ";
-        cin >> size;
-        array = new float[size];
-        arr::fill_man(size, array);
-        break;
-    }
-    default:{
-        cout << "Некорректный выбор"; 
-        return 1;
-    }
-    }
-    cout << "Массив:\n";
-    arr::print_array(array, size);
-    cout << "Ответ: " << arr::get_num(array, size) << endl;
+
     
-    choice = 0;
-    cout << "Что сделать дальше:\n1. Записать массив в файл\n2. Выход\n";
-    cin >> choice;
-    
-    switch (choice){
-
-    //Запись в файл
-    case 1:{
-        int fchoice = 0;
-        cout << "1. Запись в текстовый файл\n2. Запись в бинарный файл\n";
-        cin >> fchoice; 
-        switch (fchoice){
-        case 1:
-            cout << "Введите название файла: ";
-            cin >> file_name; 
-            try{
-                arr::print_to_file(array, size, file_name);
-            }
-            catch(const runtime_error error){
-                cout << error.what() << '\n';
-                return 1;
-            }
-            break;
-        case 2:
-            cout << "Введите название файла: ";
-            cin >> file_name; 
-            try{
-                arr::print_to_bin_file(array, size, file_name);
-            }
-            catch(const runtime_error error){
-                cout << error.what() << '\n';
-                return 1;
-            }
-            break;
-        default:
-            cout << "Некорректный выбор"; 
-            break;
-        }
-        break;
-    }
-
-    //Выход
-    default: 
-        break;
-    }
-
+    cout << arr::get_num(array, size);
     return 0;
 }
